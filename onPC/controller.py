ede7593fc5a65cd10aa2dcf34f20def3b784accf
Jussi Rasku <jussi.rasku@gmail.com>
2017-03-04 18:03:30 +0200

    facetrack

--- onPC/controller.py
+++ onPC/controller.py
@@ -7,6 +7,7 @@ import pygame
 import os.path
 import sys
 
+FACE_HOMING_DEVICE = True
 FILE_TO_PROCESS = '/home/chip/process_me.jpg'
 TCP_IP = ''
 TCP_PORT = 5005
@@ -30,6 +31,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
+if FACE_HOMING_DEVICE:
+    import facetrack
+    facetracker = facetrack.Cv2FaceTracker()
+
 print "Waiting for a client..."
 conn, addr = s.accept()
 print '... yesss! Connection from address:', addr
@@ -37,14 +42,34 @@ print '... yesss! Connection from address:', addr
 valid_img = None 
 track_left = 0
 track_right = 0
+enable_tracking = False
 while True:
   screen.fill((white))
   if os.path.isfile(FILE_TO_PROCESS):
     try:
       img = pygame.image.load(FILE_TO_PROCESS)
       valid_img = img
+      if FACE_HOMING_DEVICE and enable_tracking:
+        track_img = cv2.imread(FILE_TO_PROCESS,0)
+        x_track, y_track = facetracker.track(track_img)
+          
+        if x_track!=None:
+          if x_track > 0.75:
+            track_left = 1
+            track_right = 0
+          elif x_track < 0.25:
+            track_right = 1
+            track_left = 0
+          else:
+            track_left = 1
+            track_right = 1
+        else:
+          track_left = 0
+          track_right = 0
+                
+              
     except:
-	  pass
+      pass
     screen.blit(img,(0,0))
     
   label = myfont.render("Press 'a' to drive left track, 's' for right", 1, (255,255,0))
@@ -64,6 +89,9 @@ while True:
         track_left = 1
       if event.key == ord("s"):
         track_right = 1
+      if event.key == ord("e"):
+        enable_tracking = not enable_tracking
+      
     elif event.type == pygame.KEYUP:
       if event.key == ord("a"):
         track_left = 0
